9f2f4acbbae2ede74179c2e3b64f6a83789f2503
mauricio <mauricio@pricode.io>
2023-01-19 13:31:05 -0500

    feature/backend_model User

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,16 @@
-import app from './app.js';
-import { sequelize } from './database/database.js';
+import app from "./app.js";
+import { sequelize } from "./database/database.js";
 
 async function main() {
-    try {
-        await sequelize.authenticate();
-        console.log("connection has been established succesfully");
-        app.listen(4000);
-        console.log("server is connecting", 4000);
-    } catch (error) {
-        console.log("connection has been established succesfully", error);
-    }
+  try {
+    // await sequelize.authenticate(); //solo para probar que hay conexion
+    await sequelize.sync(); //sincroniza con la db para crear las tablas a partir del modelo importado arriba
+    console.log("connection has been established succesfully");
+    app.listen(process.env.SERVER_NODE_PORT);
+    console.log("server is connecting", process.env.SERVER_NODE_PORT);
+  } catch (error) {
+    console.log("connection has been established succesfully", error);
+  }
 }
 
 main();

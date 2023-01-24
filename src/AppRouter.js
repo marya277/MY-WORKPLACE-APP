ad4eba0ed14e05abe98f0a371df1d70e0ec8271f
mauricio <mauricio@pricode.io>
2023-01-24 18:42:45 -0500

    feature/page_candidates test recluter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -6,17 +6,21 @@ import Services from "./components/Services.js";
 import WhyUs from "./components/WhyUs.js";
 import About from "./components/About.js";
 import Careers from "./components/Careers.js";
+import Candidates from "./components/Candidates.js";
 const AppRouter = () => {
   return (
     <>
-      <Navbar />
-
+      <Navbar logged="candidate" /> {/* para probar el navbar de recluter cambiar el string por recluter
+ */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Services" element={<Services />} />
         <Route path="/WhyUs" element={<WhyUs />} />
         <Route path="/About" element={<About />} />
         <Route path="/Careers" element={<Careers />} />
+
+        {/* Candidates */}
+        <Route path="/Candidates" element={<Candidates />} />
       </Routes>
     </>
   );

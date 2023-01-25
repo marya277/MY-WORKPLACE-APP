86d2eb84c0d080ecc586423f028e0c565f91add8
AzucenaMejia <blazu.mejia11@gmail.com>
2023-01-25 16:12:20 -0500

    Home cambio en un componente.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,11 @@ import React from 'react'
 export const Home = () => {
   return (
     <div>
-        <h1> Jop Opportunities</h1>
-         <p>Great products are built block by block. MY WORLD PLACE  is redefining how products are built, applying a combination of technology and process improvements to deliver a new kind of distributed development experience.</p>
-         <h3> We got three main products:</h3>
+      <meta name="viewport" content="width=device-width,initial-scale=1"/>
+      <h1> Jop Opportunities</h1>
+        <p class="text1">Great products are built block by block. MY WORLD PLACE  is redefining how products are built, applying a combination of technology and process improvements to deliver a new kind of distributed development experience.</p>
+        <h3> We got three main products:</h3>
+        <p className='subdivition'>aqui va el texto</p>
     </div>
   )
 }

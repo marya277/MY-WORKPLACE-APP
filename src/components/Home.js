41995d32fbf5e79ce78763460a2ec0f003418ef3
AzucenaMejia <blazu.mejia11@gmail.com>
2023-01-25 20:10:11 -0500

    message: organizando clases para comenzar con las tarjetas.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,20 @@ import './Home.css';
 
 export const Home = () => {
   return (
-    <div>
+    <div className='title'>
       <meta name="viewport" content="width=device-width,initial-scale=1"/>
       <h1> Jop Opportunities</h1>
         <p class="text1">Great products are built block by block. MY WORLD PLACE  is redefining how products are built, applying a combination of technology and process improvements to deliver a new kind of distributed development experience.</p>
         <h3> We got three main products:</h3>
-        <p className='subdivition'>aqui va el texto</p>
+    <div className='inicial'>
+      <p className='subdivition'>aqui va el texto</p>
+    <div className="target">
+      <title>Senior Frontend Developer</title>
+      <p className='content'>We are looking for a highly capable Frontend Developer to work on web-based applications. You will be collaborating with international teams, designing components, and integrating data storage and protection solutions.</p>
+    </div>
+    </div>
     </div>
   )
+    
 }
 export default Home;

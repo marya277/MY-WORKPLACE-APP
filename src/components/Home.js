cbb98d3665f89adc08e628c3bfb3bb2322bd0512
AzucenaMejia <blazu.mejia11@gmail.com>
2023-01-25 20:56:27 -0500

    agregado a home targeta #uno con hipervinculo, texto y botones.

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,14 +8,16 @@ export const Home = () => {
       <h1> Jop Opportunities</h1>
         <p class="text1">Great products are built block by block. MY WORLD PLACE  is redefining how products are built, applying a combination of technology and process improvements to deliver a new kind of distributed development experience.</p>
         <h3> We got three main products:</h3>
-    <div className='inicial'>
-      <p className='subdivition'>aqui va el texto</p>
-    <div className="target">
-      <title>Senior Frontend Developer</title>
-      <p className='content'>We are looking for a highly capable Frontend Developer to work on web-based applications. You will be collaborating with international teams, designing components, and integrating data storage and protection solutions.</p>
+    
+    <div className="card">
+      <h5 className='title'>Senior Frontend Developer</h5>
+      <p className='content'>We are looking for a highly capable Frontend Developer to work on web-based applications. You will be collaborating with international teams, designing components, and integrating data storage and protection solutions.</p> 
+      <a href="Card link" className="card-link">Card link</a>
+      <a href="botton-one" className="btn btn-secondary">Apply</a>
+      <a href="botton-two" className="btn btn-secondary">Refer</a>
+      </div>
     </div>
-    </div>
-    </div>
+
   )
     
 }

5c70e2ff77a17145e5c474fcffa291bd7da1a121
AzucenaMejia <blazu.mejia11@gmail.com>
2023-01-30 16:43:02 -0500

    msg: Se añaden los ultimos cuadros, se realizan pequeños cambios en las tarjetas y se añade boostrap en el diseño.

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,32 +9,81 @@ export const Home = () => {
       <p className="text-center p-1 fs-5 ">Great products are built block by block. MY WORLD PLACE  is redefining how products are built, applying a combination of technology and process improvements to deliver a new kind of distributed development experience.</p>
       <h5 className="text-center  mb-0 p-1 fs-4"> :We got three main products</h5>
     </div>
+    <h2>
         <nav className="text-center mb-0 p-3 fs-5">
             <a href="#" className="btn btn-danger">Dedicated Product Team </a>
             <a href="#" className="btn btn-danger">Product Quality Automation</a>
             <a href="#" className="btn btn-danger">Product co-creation & blockchain </a>
         </nav>
+    </h2>
     <div className="row" style="max-width: 85rem;">
         <div className="col-sm-6">
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <h5 className="card-title " id="cardtitle">Senior Frontend Developer</h5>
+              <p className="card-text text-center">We are looking for a highly capable Frontend Developer to work on web-based applications. You will be collaborating with international teams, designing components, and integrating data storage and protection solutions.</p>
+              <a href="#" className="btn btn-primary">Refer</a>
+              <a href="#" className="btn btn-primary">Apply</a>
+              <a href="#" className=" card-link">View more</a>
             </div>
           </div>
         </div>
         <div className="col-sm-6">
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <h5 className="card-title" id="cardtitle">Senior Frontend Developer</h5>
+              <p className="card-text text-center">We are looking for a highly capable Frontend Developer to work on web-based applications. You will be collaborating with international teams, designing components, and integrating data storage and protection solutions.</p>
+              <a href="#" className="btn btn-primary">Refer</a>
+              <a href="#" className="btn btn-primary">Apply</a>
+              <a href="#" className="card-link">View More</a>
+            </div>
+          </div>
+        </div>  
+      <div className="col-sm-6">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title" id="cardtitle">Senior Frontend Developer</h5>
+              <p className="card-text text-center">We are looking for a highly capable Frontend Developer to work on web-based applications. You will be collaborating with international teams, designing components, and integrating data storage and protection solutions.</p>
+              <a href="#" className="btn btn-primary">Refer</a>
+              <a href="#" className="btn btn-primary">Apply</a>
+              <a href="#" className="card-link">View More</a>
+            </div>
+          </div>
+      </div>
+      <div className="col-sm-6">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title" id="cardtitle">Senior Frontend Developer</h5>
+              <p className="card-text text-center">We are looking for a highly capable Frontend Developer to work on web-based applications. You will be collaborating with international teams, designing components, and integrating data storage and protection solutions.</p>
+              <a href="#" className="btn btn-primary">Refer</a>
+              <a href="#" className="btn btn-primary">Apply</a>
+              <a href="#" className=" card-link">View more</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title" id="cardtitle">Senior Frontend Developer</h5>
+              <p className="card-text text-center">We are looking for a highly capable Frontend Developer to work on web-based applications. You will be collaborating with international teams, designing components, and integrating data storage and protection solutions.</p>
+              <a href="#" className="btn btn-primary">Refer</a>
+              <a href="#" className="btn btn-primary">Apply</a>
+              <a href="#" className=" card-link">View more</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title" id="cardtitle">Senior Frontend Developer</h5>
+              <p className="card-text text-center">We are looking for a highly capable Frontend Developer to work on web-based applications. You will be collaborating with international teams, designing components, and integrating data storage and protection solutions.</p>
+              <a href="#" className="btn btn-primary">Refer</a>
+              <a href="#" className="btn btn-primary">Apply</a>
             </div>
           </div>
         </div>
       </div>
-</div>
+    </div>
   )
     
 }
